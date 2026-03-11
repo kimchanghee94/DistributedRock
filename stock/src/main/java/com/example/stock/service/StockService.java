d863d091b0360bf0c0538092d05091287ad0c48f
kimchanghee <kimchanghee94@naver.com>
2026-03-11 12:32:02 +0900

    #main 1. Synchronized자체의 문제점

--- stock/src/main/java/com/example/stock/service/StockService.java
+++ stock/src/main/java/com/example/stock/service/StockService.java
@@ -11,7 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class StockService {
     private final StockRepository stockRepository;
 
-//    @Transactional
+/*    public synchronized void decrease(Long id, Long quantity){
+    Synchronized를 사용하게 될 경우 단일 서버에서 처리된다면 상관없지만
+    수평적확대로 처리된 서버 2개 이상에 처리되면 데이터 접근을 여러대에서 접근가능해진다
+ */
+    @Transactional
     public synchronized void decrease(Long id, Long quantity){
         //Stock 조회
         //재고를 감소
